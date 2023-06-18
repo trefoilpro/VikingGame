c71eefda5bbf11d7ef45147e1e42f1b42dbc63b6
Roman Protsai <protsayroman228@gmail.com>
2023-06-19 00:48:38 +0300

    UI scene FirstVariant and SecondVariant

--- VikingPlay/Assets/Scripts/UI/MainMenuBackGround.cs
+++ VikingPlay/Assets/Scripts/UI/MainMenuBackGround.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
@@ -10,7 +8,7 @@ public class MainMenuBackGround : MonoBehaviour
     [SerializeField] private Camera _camera;
     
     private float _radius = 200f; 
-    private float _duration = 10f;
+    private float _duration = 60f;
     private Sequence _cameraAnimation;
     
    
@@ -45,30 +43,4 @@ public class MainMenuBackGround : MonoBehaviour
     {
         _cameraAnimation.Kill();
     }
-
-    /*IEnumerator BackGroundFlyingCamera()
-    {
-        Vector3 firstPosition = _mapCenter.transform.position + new Vector3(0, 120f, -_radius);
-        Vector3 secondPosition = _mapCenter.transform.position + new Vector3(-_radius, 120f, 0);
-        Vector3 thirdPosition = _mapCenter.transform.position + new Vector3(0, 120f, _radius);
-        Vector3 fourthPosition = _mapCenter.transform.position + new Vector3(_radius, 120f, 0);
-
-        Vector3[] pathPoints = { firstPosition, secondPosition, thirdPosition, fourthPosition, firstPosition };
-
-        if (Vector3.Distance(_camera.transform.position, firstPosition) > 0.1f)
-        {
-            _camera.transform.DOMove(firstPosition, 1f).SetEase(Ease.Linear);
-            yield return new WaitForSeconds(1f);
-        }
-
-        _camera.transform.DOPath(pathPoints, _duration, PathType.CatmullRom).SetEase(Ease.Linear);
-        
-        _camera.transform.DORotate(_camera.transform.rotation.eulerAngles + new Vector3(0, 180f, 0), _duration / 2).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(_duration / 2);
-        _camera.transform.DORotate(_camera.transform.rotation.eulerAngles + new Vector3(0, 180f, 0), _duration / 2).SetEase(Ease.Linear);
-        yield return new WaitForSeconds(_duration / 2);
-
-        yield return StartCoroutine(BackGroundFlyingCamera());
-
-    }*/
 }
---
+++ VikingPlay/Assets/Scripts/UI/UISceneScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UISceneScript : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
